f68dc7494c81438f845b0d1b8fcd91afa76fd9e4
kai-dg <km.commissions@gmail.com>
2019-05-09 21:51:34 +0000

    turned off redirects

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """returns first 10 posts"""
     header = {"User-Agent": "Holberton"}
     url = "https://www.reddit.com/r/" + subreddit + "/hot.json?limit=10"
-    r = requests.get(url, headers=header)
+    r = requests.get(url, headers=header, allow_redirects=False)
     if r.status_code == 200:
         for i in r.json().get("data", None).get("children", None):
             print(i.get("data", None).get("title", None))
